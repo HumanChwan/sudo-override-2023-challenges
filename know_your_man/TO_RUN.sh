4c4afe11559167af4e0702d43213760cfc5af236
Dinesh Chukkala <dineshchukkala169@gmail.com>
2023-01-27 12:43:35 +0530

    made edits to know_your_man [know_your_man]

--- know_your_man/TO_RUN.sh
+++ know_your_man/TO_RUN.sh
@@ -1,14 +1,24 @@
+#!/bin/bash
+
+function exit_code () {
+    if [ $? -ne 0 ];
+    then
+        echo "poo poo: at $1"
+    fi
+}
+
+docker stop know_your_man
+exit_code "stopping"
+
+docker rm `docker ps -a | grep 'know_your_man' | head -n 1 | cut -d ' ' -f1`
+exit_code "removing ps"
+
+docker rmi -f know_your_man
+exit_code "removing image"
+
 docker build -t know_your_man .
-if [ $? -ne 0 ];
-then
-    echo "poo poo: at build"
-    exit 1
-fi
+exit_code "build"
 
 # TODO: decide the port (keeping it 4545 for now)
 docker run -d -p 4545:22 know_your_man
-if [ $? -ne 0 ];
-then
-    echo "poo poo: at run"
-    exit 1
-fi
+exit_code "run"
